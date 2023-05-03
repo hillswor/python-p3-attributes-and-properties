ad4be5ccc151582b0aba76e013d2942cd7b42507
Bruce <bruce.hillsworth@gmail.com>
2023-05-02 20:54:12 -0600

    Passed tests

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+import ipdb
+
 
 APPROVED_BREEDS = [
     "Mastiff",
@@ -8,8 +10,36 @@ APPROVED_BREEDS = [
     "Beagle",
     "French Bulldog",
     "Pug",
-    "Pointer"
+    "Pointer",
 ]
 
+
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Corgi"):
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if type(name) != str or len(name) > 25 or len(name) < 1:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = name
+
+    def get_breed(self):
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            self._breed = breed
+
+    name = property(get_name, set_name)
+
+    breed = property(get_breed, set_breed)
+
+
+# ipdb.set_trace()
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import ipdb
 
 APPROVED_JOBS = [
     "Admin",
@@ -12,8 +13,33 @@ APPROVED_JOBS = [
     "General Management",
     "Research & Development",
     "Marketing",
-    "Purchasing"
+    "Purchasing",
 ]
 
+
 class Person:
-    pass
+    def __init__(self, name="John Doe", job="Sales"):
+        self.name = name
+        self.job = job
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        if type(name) != str or len(name) > 25 or len(name) < 1:
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = name.title()
+
+    def get_job(self):
+        return self._job
+
+    def set_job(self, job):
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = job
+
+    name = property(get_name, set_name)
+
+    job = property(get_job, set_job)
